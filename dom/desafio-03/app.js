b455aadffab167079879cf70f9b5e362c5723cc0
Douglas Silva <doug.neo@gmail.com>
2021-01-15 15:16:13 -0300

    Adicionado mais atividades

---
+++ dom/desafio-03/app.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        valor: 0,
+    },
+    watch: {
+        valor(novo, antigo) {
+            setTimeout(() => {
+                this.valor = 0
+            }, 5000)
+        }
+    },
+    computed: {
+        resultado() {
+            return this.valor == 37 ? 'Valor Igual' : 'Valor Diferente'
+        }
+
+    }
+
+});
