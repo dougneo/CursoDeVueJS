117f44c9f23cf62efeb1e19f0d209633d3d7bb1e
Douglas Silva <doug.neo@gmail.com>
2021-01-19 14:59:01 -0300

    Adicionado novas atividades

--- matador-de-monstros/app.js
+++ matador-de-monstros/app.js
@@ -1,6 +1,34 @@
-var barraJogador = new VTTCue({
-    el: '#jogador',
+new Vue({
+    el: '#jogo',
     data: {
-
-    }
+        newGame: true,
+        lifeJogador: 100,
+        lifeMonstro: 100,
+        
+    },
+    methods: {
+        ataque() {
+            let atkjogador = Math.round(10 * Math.random())
+            let atkmonstro = Math.round(6 * Math.random())
+            this.lifeJogador -= atkjogador
+            this.lifeMonstro -= atkmonstro
+        },
+        ataqueespecial() {
+            let atkjogador = Math.round(6 * Math.random())
+            let atkmonstro = Math.round(10 * Math.random())
+            this.lifeJogador -= atkjogador
+            this.lifeMonstro -= atkmonstro
+        },
+        curar() {
+            let curarjogador = Math.round(10 * Math.random())
+            let atkmonstro = Math.round(10 * Math.random())
+            this.lifeJogador += curarjogador
+            this.lifeMonstro -= atkmonstro
+        },
+        desistir() {
+            let reset = 100
+            this.lifeJogador = reset
+            this.lifeMonstro = reset
+        }
+    },
 })
